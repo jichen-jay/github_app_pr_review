0dde984e1f3d11fbe5b29c3f1a7ccd2b7743b38a
jaykchen <jaykchen@icloud.com>
2024-10-08 16:19:53 -0400

    deserialize webhook payload with octo lib

--- tester_hook/src/lib.rs
+++ tester_hook/src/lib.rs
@@ -2,7 +2,18 @@ use dotenv::dotenv;
 use flowsnet_platform_sdk::logger;
 // use github_app_pr_review::{fetch_and_review_files};
 // use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use github_flows::{
+    // event_handler, get_octo, listen_to_event,
+    octocrab::models::webhook_events::payload::PullRequestWebhookEventPayload,
+    // octocrab::models::Author,
+    // octocrab::models::webhook_events::payload::{
+    //     PullRequestWebhookEventAction, PullRequestWebhookEventPayload,
+    // },
+    // octocrab::models::webhook_events::{WebhookEvent, WebhookEventPayload},
+    // octocrab::models::CommentId,
+    // GithubLogin,
+};
+use serde_json::{self, Value};
 use std::collections::HashMap;
 use webhook_flows::{create_endpoint, request_handler};
 
@@ -22,7 +33,34 @@ async fn handler(
     logger::init();
     dotenv().ok();
 
-    let load = match String::from_utf8(_body) {
+    // let load = match String::from_utf8(_body) {
+    //     Ok(obj) => obj,
+    //     Err(_e) => {
+    //         log::error!("failed to parse body: {}", _e);
+    //         panic!("failed to parse body");
+    //     }
+    // };
+
+    // log::info!("payload dump: {:?}", load);
+
+    let _ = log_them(&_body).await;
+    // match serde_json::to_string_pretty(&load) {
+    //     Ok(pretty_json) => log::info!("Received webhook payload: {}", pretty_json),
+    //     Err(e) => log::error!("Failed to deserialize webhook payload: {}", e),
+    // }
+}
+
+// pub async fn parse_incoming_payload() {
+
+//     let octo = get_octo(&GithubLogin::Default);
+//     let issues = octo.issues(owner.clone(), repo.clone());
+
+//     let pulls = octo.pulls(owner.clone(), repo.clone());
+
+// }
+
+pub async fn log_them(_body: &[u8]) {
+    let load = match serde_json::from_slice::<PullRequestWebhookEventPayload>(&_body) {
         Ok(obj) => obj,
         Err(_e) => {
             log::error!("failed to parse body: {}", _e);
@@ -30,12 +68,30 @@ async fn handler(
         }
     };
 
-    // match serde_json::to_string_pretty(&hook_payload_struct) {
-    //     Ok(pretty_json) => log::info!("Received webhook payload: {}", pretty_json),
-    //     Err(e) => log::error!("Failed to deserialize webhook payload: {}", e),
-    // }
+    let owner = load.pull_request.user.as_ref()
+        .and_then(|user| Some(user.login.as_ref()))
+        .unwrap_or("unknown_user"); // Provide a default if none
 
-    log::info!("payload dump: {:?}", load);
+    let repo = load.pull_request.repo.as_ref()
+        .and_then(|repository| Some(repository.name.as_ref()))
+        .unwrap_or("unknown_repo"); // Provide a default if none
+
+    // let owner_what = load.pull_request.repo.as_ref()
+    //     .and_then(|repository| Some(repository.owner.as_ref()))
+    //     .unwrap_or("failed to get author");
+
+    // let repo_what = load.pull_request.user.as_ref()
+    // .and_then(|user| Some(user.repos_url.as_ref()))
+    //     .unwrap_or("unknown_repos_url"); // Provide a default if none
+
+    let pull_number = load.number;
+    
 
 
+    log::info!("pr owner: {:?}", owner);
+    log::info!("repo from pr: {:?}", repo);
+    // log::info!("owner what: {:?}", owner_what);
+    // log::info!("repo what: {:?}", repo_what);
+    log::info!("pull number: {:?}", pull_number);
+    log::info!("parsed payload: {:?}", load);
 }
